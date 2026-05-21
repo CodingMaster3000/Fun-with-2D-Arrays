8d45357b4e891e68a90730bf538124a8c514b556
CodingMaster3000 <67920617+CodingMaster3000@users.noreply.github.com>
2026-05-21 16:49:15 +0200

    asking wether cells should have alternating values
    asks for user input about whether he prefers alternating or non-alternating cell values, update + prints the grid depending on the answer

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             int gridSize;
-
+            int alternatingFactor;
             Console.WriteLine("You can create a grid. What size should it have?");
-            string input  = Console.ReadLine();
-            gridSize = Convert.ToInt32(input);
+            string gridSizeInput = Console.ReadLine();
+            gridSize = Convert.ToInt32(gridSizeInput);
             string[,] grid = new string[gridSize, gridSize];
             for (int i = 0; i < gridSize; i++)
             {
@@ -25,6 +25,32 @@
                     Console.Write(" ");
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("Should the cells of the grid have alternating values? (y/n)");
+            string modealternatingInput = Console.ReadLine().ToLower();
+            if (modealternatingInput == "y")
+            {
+                for (int i = 0; i < gridSize; i++)
+                {
+                    for (int j = 0; j < gridSize; j++)
+                    {
+                        alternatingFactor = Convert.ToInt32(Math.Pow(-1, i + 1) * Math.Pow(-1, j));
+                        if (alternatingFactor == 1)
+                        {
+                            grid[i, j] = "X";
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < gridSize; i++)
+                {
+                    for (int j = 0; j < gridSize; j++)
+                    {
+                        Console.Write($"{grid[i, j]}");
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
             }
         }
     }
